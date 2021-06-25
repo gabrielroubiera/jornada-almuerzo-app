10694369a6bfc9b11abd16120ae6035d51925dc5
Gabriel A. Rodriguez Ubiera <grodriguezubiera@gmail.com>
2021-06-25 17:02:50 -0400

    fix cors

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
+
+app.set('port', process.env.PORT || 5000);
+app.use(cors());
 app.use(express.static(__dirname + '/dist/jornada-almuerzo-app'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/jornada-almuerzo-app/index.html'));
 });
-app.listen(process.env.PORT || 8000);
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'))
+});
